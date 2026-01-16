2c585f095d1d2c66bee2599fbe4d627a60d84d3b
pr0methevs <artxk92@gmail.com>
2026-01-15 20:11:54 -0500

    feat(testing): add jest testing setup with ts-jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+export default {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+    // If you store tests in a specific folder, you can specify it here:
+    // testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+    verbose: true,
+};
